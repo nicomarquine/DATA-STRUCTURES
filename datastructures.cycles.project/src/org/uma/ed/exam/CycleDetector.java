05f899ec63eec9afa1c2768659cc2a7f1009340d
nicomarquine <nicomarquine@gmail.com>
2026-01-20 20:12:18 +0100

    Para ti Josek

--- datastructures.cycles.project/src/org/uma/ed/exam/CycleDetector.java
+++ datastructures.cycles.project/src/org/uma/ed/exam/CycleDetector.java
@@ -37,16 +37,62 @@ public class CycleDetector<V> {
 
   //// BEGIN (A)
   public CycleDetector(Graph<V> graph) {
-    // Complete the algorithm here
-    throw new UnsupportedOperationException("Not implemented yet");
+    this.cyclic = false;
+    this.predecessors = new JDKHashDictionary<>();
+    this.cycle = new JDKArrayList<>();
+
+    // Grafo vacío => acíclico
+    if (graph.isEmpty()) {
+      return;
+    }
+
+    Set<V> visited = new JDKHashSet<>();
+    Stack<Pair<V>> stack = new JDKStack<>();
+
+    // Elegimos un vértice cualquiera como source
+    V source = graph.vertices().iterator().next();
+
+    // Predecesor ficticio para el vértice inicial
+    stack.push(Pair.of(source, source));
+
+    while (!cyclic && !stack.isEmpty()) {
+      Pair<V> pair = stack.top();
+      stack.pop();
+      V predecessor = pair.predecessor();
+      V current = pair.current();
+
+      if (visited.contains(current)) {
+        cyclic = true;
+        reconstructCycle(predecessor, current);
+      } else {
+        visited.insert(current);
+        predecessors.insert(current, predecessor);
+
+        for (V successor : graph.successors(current)) {
+          if (!successor.equals(predecessor)) {
+            stack.push(Pair.of(current, successor));
+          }
+        }
+      }
+    }
   }
+
   //// END (A)
 
   //// BEGIN (B)
   // Reconstructs a cycle from the first vertex revisited and its predecessor. Stores it in cycle list
   private void reconstructCycle(V predecessor, V revisited) {
     // Complete the cycle reconstruction algorithm here
-    throw new UnsupportedOperationException("Not implemented yet");
+    cycle.clear();
+
+    cycle.append(revisited);
+    V current = predecessor;
+
+    while(!current.equals(revisited)){
+      cycle.append(current);
+      current = predecessors.valueOf(current);
+    }
+    cycle.append(revisited);
   }
   //// END (B)
 
